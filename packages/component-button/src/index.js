d7959329125bec603eb1545544e2a7ff5ec44ce4
Andrew Cashmore <andy@glanc.ee>
2017-06-23 11:53:42 +0100

    add tests

--- packages/component-button/src/index.js
+++ packages/component-button/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const Button = ({ children, onClick }) => (
-  <button onClick={onClick}>
-    <p>{children}</p>
+  <button className="button" onClick={onClick}>
+    {children}
   </button>
 )
 
